f0af631c09c3327b6a3fdf50f2abcf690833a243
Apocalipsis Torres <apocalipsisTorres@gmail.com>
2022-10-24 15:29:38 -0600

    added a template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
+from flask import render_template
 from app import app
 
 # Create a route for our app
 @app.route('/')
 def index():
-    return 'hello this is the index route'
+    user_info = {
+        'username': 'Apocalipsis',
+        "email": "Apocalip@gmail.com"
+        }
+    colors = ["red","blue","Silver",'Black','Burgandy','Cyan',]
+    return render_template('index.html', user=user_info, colors=colors)
+    
+
 
 @app.route('/posts')
 def posts():
